8f53eecf339292f916de897810eff30948c2b1a7
Viktor Birgisson <viktor.birgisson@gmail.com>
2024-02-02 14:30:49 +0000

    some fixes to blogNav and blogPost

--- components/blog/blogNav.tsx
+++ components/blog/blogNav.tsx
@@ -2,6 +2,9 @@ import { BlogNavStyle } from "@/styles/pageStyles/blog.styles";
 import { FilledButton } from "../buttons";
 import Link from "next/link";
 
+// BlogNavStyle is a style for the nav bar
+// Reused the FilledButton
+
 const BlogNav = () => {
   return (
     <>
--- components/blog/blogPosts.tsx
+++ components/blog/blogPosts.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 interface Greeting {
-  id: string; // Adjust the type according to your actual data structure
+  id: string;
   greeting: string;
 }
 
